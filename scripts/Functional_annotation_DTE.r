0943af5fdac9f512d8fa81af3c21fd1bdf4ed940
KellyCardona <cardonakelly11@gmail.com>
2024-09-12 15:59:30 +0300

    kegg_annotations

--- scripts/Functional_annotation_DTE.r
+++ scripts/Functional_annotation_DTE.r
@@ -6,19 +6,23 @@ library(org.Hs.eg.db)
 library(AnnotationDbi)
 library(ReactomePA)
 library(ggplot2)
+library(openxlsx)
 
 # Load the differentially expressed transcripts optimized
-# Define the sheet names
-sheet_names <- c("Common_up", "Common_down", "Common_up_to_down", "Common_down_to_up")  # Add all the sheet names you want to load
+# Define the sheet names, change as needed
+#sheet_names <- c("Common_up", "Common_down", "Common_up_to_down", "Common_down_to_up")  # Add all the sheet names you want to load
+sheet_names <- c("Common_up", "Common_down") 
 # Initialize an empty list to store the data frames
 data_frames <- list()
 # Column number to check for NA values
-column_number <- 13  # Change this to the appropriate column number (gene_id)
+column_number <- 24  # Change this to the appropriate column number (gene_id)
+# File with the intersections,change as needed
+file <- "../results/upset_intersections_HGA.xlsx"
 
 # Loop through the sheet names and read each sheet into a data frame
 for (sheet in sheet_names) {
   # Read the sheet into a data frame
-  df <- read.xlsx("../results/upset_intersections_iPSCs.xlsx", sheet = sheet)
+  df <- read.xlsx(file, sheet = sheet)
   # Remove rows with NA values in the specified column
   df <- df[!is.na(df[[column_number]]), ]
   # Store the data frame in the list
@@ -31,6 +35,25 @@ det_ipscs_up <- data_frames[["Common_up"]]
 det_ipscs_down <- data_frames[["Common_down"]]
 det_ipscs_up_to_down <- data_frames[["Common_up_to_down"]]
 det_ipscs_down_to_up <- data_frames[["Common_down_to_up"]]
+#NSCs
+det_nscs_up <- data_frames[["Common_up"]]
+det_nscs_down <- data_frames[["Common_down"]]
+det_nscs_up_to_down <- data_frames[["Common_up_to_down"]]
+det_nscs_down_to_up <- data_frames[["Common_down_to_up"]]
+
+#Neurons
+det_neurons_up <- data_frames[["Common_up"]]
+det_neurons_down <- data_frames[["Common_down"]]
+det_neurons_up_to_down <- data_frames[["Common_up_to_down"]]
+det_neurons_down_to_up <- data_frames[["Common_down_to_up"]]
+
+#KS
+det_ks_up <- data_frames[["Common_up"]]
+det_ks_down <- data_frames[["Common_down"]]
+
+#HGA
+det_hga_up <- data_frames[["Common_up"]]
+det_hga_down <- data_frames[["Common_down"]]
 
 #________________________________DEFINING BACKGROUND FOR ORA ANALYSIS______________________________________
 
@@ -57,53 +80,78 @@ perform_enrichment_analysis <- function(df, column_name, universe) {
   # Remove NA values
   entrez_ids <- entrez_ids[!is.na(entrez_ids)]
   
-  # Convert the entrez ids to use it in the enrichment analysis
-  entrez_ids_df <- data.frame(ensembl_gene_id = names(entrez_ids), entrez_id = entrez_ids, stringsAsFactors = FALSE)
-  
-  # Perform GO enrichment analysis
-  go_enrichment <- enrichGO(entrez_ids_df$entrez_id, OrgDb = org.Hs.eg.db, keyType = "ENTREZID", ont = "ALL", pvalueCutoff = 0.05, qvalueCutoff = 0.05, pAdjustMethod = "BH", universe = universe, readable = TRUE)
+  # Perform GO enrichment analysis for all ontologies
+  go_enrichment <- enrichGO(entrez_ids, OrgDb = org.Hs.eg.db, keyType = "ENTREZID", ont = "ALL", pvalueCutoff = 0.05, qvalueCutoff = 0.05, pAdjustMethod = "BH", universe = universe, readable = TRUE)
   
   # Perform KEGG enrichment analysis
-  kegg_enrichment <- enrichKEGG(entrez_ids_df$entrez_id, organism = "hsa", pvalueCutoff = 0.05, qvalueCutoff = 0.05, pAdjustMethod = "BH")
+  kegg_enrichment <- enrichKEGG(entrez_ids, organism = "hsa", pvalueCutoff = 0.05, qvalueCutoff = 0.05, pAdjustMethod = "BH")
   
-  # Return the results as data frames
-  return(list(go_results = as.data.frame(go_enrichment), kegg_results = as.data.frame(kegg_enrichment)))
+  # Convert the results to data frames
+  go_results_df <- as.data.frame(go_enrichment)
+  kegg_results_df <- as.data.frame(kegg_enrichment)
+  
+  # Separate the GO results by ontology
+  go_bp <- go_results_df[go_results_df$ONTOLOGY == "BP", ]
+  go_mf <- go_results_df[go_results_df$ONTOLOGY == "MF", ]
+  go_cc <- go_results_df[go_results_df$ONTOLOGY == "CC", ]
+  
+  # Return the results as a list of data frames and original enrichment objects
+  return(list(go_bp = go_bp, go_mf = go_mf, go_cc = go_cc, kegg_results = kegg_results_df, go_all =go_results_df,go_enrichment = go_enrichment, kegg_enrichment = kegg_enrichment))
 }
 
-
+# Enrichment results for iPCSs
 results_up_ipscs <- perform_enrichment_analysis(det_ipscs_up, "gene_id_KS", lowly_expressed_entrez)
 results_down_ipscs <- perform_enrichment_analysis(det_ipscs_down, "gene_id_KS", lowly_expressed_entrez)
 results_up_to_down_ipscs <- perform_enrichment_analysis(det_ipscs_up_to_down, "gene_id_KS", lowly_expressed_entrez)
-#results_down_to_up_ipscs <- perform_enrichment_analysis(det_ipscs_down_to_up, "gene_id_KS", lowly_expressed_entrez) #to few genes
+results_down_to_up_ipscs <- perform_enrichment_analysis(det_ipscs_down_to_up, "gene_id_KS", lowly_expressed_entrez)
 
-# go_results <- results$go_results
-# kegg_results <- results$kegg_results
+# Enrichment results for NSCs
+results_up_nscs <- perform_enrichment_analysis(det_nscs_up, "gene_id_KS", lowly_expressed_entrez)
+results_down_nscs <- perform_enrichment_analysis(det_nscs_down, "gene_id_KS", lowly_expressed_entrez)
+results_up_to_down_nscs <- perform_enrichment_analysis(det_nscs_up_to_down, "gene_id_KS", lowly_expressed_entrez)
+results_down_to_up_nscs <- perform_enrichment_analysis(det_nscs_down_to_up, "gene_id_KS", lowly_expressed_entrez)
+
+#Enrichment results for Neurons
+results_up_neurons <- perform_enrichment_analysis(det_neurons_up, "gene_id_KS", lowly_expressed_entrez)
+results_down_neurons <- perform_enrichment_analysis(det_neurons_down, "gene_id_KS", lowly_expressed_entrez)
+results_up_to_down_neurons <- perform_enrichment_analysis(det_neurons_up_to_down, "gene_id_KS", lowly_expressed_entrez)
+results_down_to_up_neurons <- perform_enrichment_analysis(det_neurons_down_to_up, "gene_id_KS", lowly_expressed_entrez)
+
+#Enrichment results for KS
+results_up_ks <- perform_enrichment_analysis(det_ks_up, "gene_id", lowly_expressed_entrez)
+results_down_ks <- perform_enrichment_analysis(det_ks_down, "gene_id", lowly_expressed_entrez)
+
+#Enrichment results for HGA
+results_up_hga <- perform_enrichment_analysis(det_hga_up, "gene_id", lowly_expressed_entrez)
+results_down_hga <- perform_enrichment_analysis(det_hga_down, "gene_id", lowly_expressed_entrez)
 
 
+# Save the results of those that produced significant enrichment in iPCSs
+write.xlsx(results_up_to_down_ipscs$kegg_results, file = "../results/KEGG_up_to_down_iPSCs.xlsx")
+# Plot the results from the KEGG enrichment analysis in the significant in iPCSs
+dotplot(results_up_to_down_ipscs$kegg_enrichment, showCategory = 10)
+# Save the plot
+ggsave("./plots/KEGG_up_to_down_iPSCs.png")
 
 
+# Save the results of those that produced significant enrichment in NSCs
+write.xlsx(results_up_nscs$kegg_results, file = "../results/KEGG_common_up_NSCs.xlsx")
+write.xlsx(results_down_nscs$kegg_results, file = "../results/KEGG_common_down_NSCs.xlsx")
+# Plot the results from the KEGG enrichment analysis in the significant in NSCs
+dotplot(results_up_nscs$kegg_enrichment, showCategory = 10)
+dotplot(results_down_nscs$kegg_enrichment, showCategory = 10)
+# Save the plot
+ggsave("./plots/KEGG_common_up_NSCs.png")
+ggsave("./plots/KEGG_common_down_NSCs.png")
 
+#Nothing significant in Neurons
 
+# Save the results of those that produced significant enrichment in KS
+write.xlsx(results_up_ks$kegg_results, file = "../results/KEGG_common_up_KS.xlsx")
+#plot the results from the KEGG enrichment analysis in the significant in KS
+dotplot(results_up_ks$kegg_enrichment, showCategory = 10)
+#save the plot
+ggsave("./plots/KEGG_common_up_KS.png")
 
-
-
-
-
-
-#________________________________GO AND KEGG ENRICHMENT ANALYSIS______________________________________
-
-# Map the gene ids of the differentially expressed transcripts to entrez ids
-entrez_ids <- mapIds(org.Hs.eg.db, keys = det_ipscs_up$gene_id_KS, column = "ENTREZID", keytype = "ENSEMBL",multiVals = "first")
-# Remove NA values
-entrez_ids <- entrez_ids[!is.na(entrez_ids)]
-# Convert the entrez ids to use it in the enrichment analysis
-entrez_ids_df <- data.frame(ensembl_gene_id = names(entrez_ids), entrez_id = entrez_ids, stringsAsFactors = FALSE)
-
-# Perform GO enrichment analysis
-go_enrichment <- enrichGO(entrez_ids_df$entrez_id, OrgDb = org.Hs.eg.db, keyType = "ENTREZID", ont = "ALL", pvalueCutoff = 0.05, qvalueCutoff = 0.05, pAdjustMethod = "BH", universe = lowly_expressed_entrez, readable = TRUE)
-# visualise the results
-dotplot(go_enrichment, showCategory = 10)
-# Perform KEGG enrichment analysis
-kegg_enrichment <- enrichKEGG(entrez_ids_df$entrez_id, organism = "hsa", pvalueCutoff = 0.05, qvalueCutoff = 0.05, pAdjustMethod = "BH")
-# visualise the results
-dotplot(kegg_enrichment, showCategory = 10)
+# Save the results of those that produced significant enrichment in HGA
+write.xlsx(results_up_hga$kegg_results, file = "../results/KEGG_common_up_HGA.xlsx")
