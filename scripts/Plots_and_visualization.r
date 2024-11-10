0b3409334db3aa05571f660903e7eb2def5aa89f
KellyCardona <cardonakelly11@gmail.com>
2024-11-10 15:35:37 +0300

    plots xci status

--- scripts/Plots_and_visualization.r
+++ scripts/Plots_and_visualization.r
@@ -6,6 +6,7 @@ library(ggpubr)
 library(openxlsx)
 library(ggrepel)
 library(dplyr)
+library(forcats)
 
 # Load data: DTE results
 names_sheets <- c("KS_iPSCs", "HGA_iPSCs", "KS_NSCs", "HGA_NSCs", "KS_Neurons", "HGA_Neurons")
@@ -204,3 +205,62 @@ plot_log2FC_by_chromosome(ks_nscs_res, "./Isoforms-Splicing_Bulk/plots/log2FC_de
 plot_log2FC_by_chromosome(hga_nscs_res, "./Isoforms-Splicing_Bulk/plots/log2FC_density_hga_nscs.png")
 plot_log2FC_by_chromosome(ks_neurons_res, "./Isoforms-Splicing_Bulk/plots/log2FC_density_ks_neurons.png")
 plot_log2FC_by_chromosome(hga_neurons_res, "./Isoforms-Splicing_Bulk/plots/log2FC_density_hga_neurons.png")
+
+#PIE CHART TO SHOW THE PROPORTION OF XCI STATUS
+
+create_xci_pie_chart <- function(data, file_name = "xci_pie_chart.png") {
+  
+  # Filter out transcripts categorized as "ns" and on chromosome X
+  filtered_data <- data %>%
+    filter(DE != "NS" & chromosome == "X")
+  
+  # Categorize transcripts by XCI status
+  dte_categories <- filtered_data %>%
+    mutate(category = case_when(
+      XCI_status == "escapes" ~ "escapes",
+      XCI_status == "inactive" ~ "inactive",
+      XCI_status == "variable" ~ "variable",
+      XCI_status == "PAR" ~ "PAR",
+      is.na(XCI_status) ~ "NA",
+      TRUE ~ "Other"
+    )) %>%
+    filter(category != "Other") %>%
+    count(category, name = "value") %>%
+    mutate(proportion = value / sum(value) * 100) # Calculate percentages
+  
+  # Calculate positions for the labels
+  dte_categories <- dte_categories %>%
+    mutate(csum = rev(cumsum(rev(proportion))), # Cumulative sum of proportions
+           pos = proportion / 2 + lead(csum, 1), # Midpoint for each slice
+           pos = if_else(is.na(pos), proportion / 2, pos)) # Handle NA for the last position
+  
+  # Create the pie chart
+  plot <- ggplot(dte_categories, aes(x = "", y = proportion, fill = fct_inorder(category))) +
+    geom_col(width = 1, color = 1) + # Pie chart slices
+    geom_text(aes(label = paste0(round(proportion, 1), "%")),
+              position = position_stack(vjust = 0.5)) + # Labels inside slices
+    coord_polar(theta = "y") + # Convert to polar coordinates
+    guides(fill = guide_legend(title = "Category")) + # Legend for categories
+    scale_y_continuous(breaks = dte_categories$pos, labels = dte_categories$category) + # Align labels
+    theme(axis.ticks = element_blank(),
+          axis.title = element_blank(),
+          axis.text = element_text(size = 12), 
+          legend.position = "none", # Remove legend
+          panel.background = element_rect(fill = "white")) # White background
+  
+  # Save the plot
+  ggsave(filename = file_name, plot = plot, width = 6, height = 6, dpi = 300)
+}
+
+# ks ipscs
+create_xci_pie_chart(res_KS_iPSCs_annotated, "./plots/ks_ipscs_xci_status.png")
+# hga ipscs
+create_xci_pie_chart(res_HGA_iPSCs_annotated, "./plots/hga_ipscs_xci_status.png")
+# ks nscs
+create_xci_pie_chart(res_KS_NSCs_annotated, "./plots/ks_nscs_xci_status.png")
+# hga nscs
+create_xci_pie_chart(res_HGA_NSCs_annotated, "./plots/hga_nscs_xci_status.png")
+# ks neurons
+create_xci_pie_chart(res_KS_Neurons_annotated, "./plots/ks_neurons_xci_status.png")
+# hga neurons
+create_xci_pie_chart(res_HGA_Neurons_annotated, "./plots/hga_neurons_xci_status.png")
